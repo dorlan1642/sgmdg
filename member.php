f3b12cddaffb6063726edb8298d18b5c21ca873f
Flavio <flavio.almanza01@gmail.com>
2019-09-13 18:18:44 -0600

    Change member.php and members.php

--- member.php
+++ member.php
@@ -1,12 +1,27 @@
-
 <?php
-include "parts/_header.php";
+include "./parts/_header.php";
 ?>
 
+<?php
+    include './admin/php/conexion.php';
+    $id=$_GET['id'];
+    $resultado=$mysqli->query("select * from Members where id='$id'")or die($mysqli->error);
+        while ($fila=mysqli_fetch_array($resultado)){
+?>
 
+<div class="container ">
+    <img class="img-thumbnail center-block  img-responsive member-img" src="./img/img_members/<?php echo $fila['url_img']; ?>" alt="">
+    <div class="gray-lighter ">
+        <h1 class="member-h1"> <?php echo $fila['name']; ?> </h1>
+        <h1 class="member-h2"><?php echo $fila['resume']; ?></h1>
+        <p class="member-p"><?php echo $fila['content']; ?></p>
+    </div>
+</div>
 
+<?php
+}
+?>
 
 <?php
 include 'parts/_footer_main.php'
 ?>
-</html>
--- members.php
+++ members.php
@@ -3,30 +3,29 @@ include "parts/_header.php";
 ?>
 
 <div class="container">
-	<div class="row">
+	<div class="row padding">
 	<?php 
-		include "./php/conexion.php";
+		include "./admin/php/conexion.php";
 		$resultado=$mysqli->query("select * from Members")or die ($mysqli->error);
 			while ($fila= mysqli_fetch_array($resultado)) {
 	?>
-		<div class="gray-lighter ">
+
+		<div class="gray-lighter">
 			<div class=" col-xs-12 col-sm-6 col-md-4  ">
 				<a href="./event.php?id=<?php echo $fila['id']; ?>">
-					<img class="img-thumbnail center-block  img-responsive marg-img " src="./img/img_events/<?php echo $fila['url_img']; ?>" alt="">
-				<div>
-					<h3>
-					<?php echo $fila['date_event']; ?>
-					</h3>
-					<h5> <?php echo $fila['resume']; ?> </h5>
-				</div>
+					<img class="img-thumbnail center-block  img-responsive marg-img image-focus" src="./img/img_events/<?php echo $fila['url_img']; ?>" alt="">
+					<div>
+						<h4 class="index-h4"><?php echo $fila['name']; ?></h4>
+						<p class="index-p"> <?php echo $fila['resume']; ?> </p>
+					</div>
 				</a>
 			</div>
 		</div>
 	<?php 
 } 
 ?>
+</div>
 
 <?php
 include 'parts/_footer_main.php'
 ?>
-</html>
